a446cbe14f51890bdb4e20900b4cdd14dac894b6
Reyna Javar <rjavar@catalyte.io>
2021-02-12 19:46:11 -0800

    spent 2 hours on home page button

--- wheredoyouwanttoeat/src/Components/Homepage.jsx
+++ wheredoyouwanttoeat/src/Components/Homepage.jsx
@@ -1,27 +1,12 @@
 import logo from '../logo.svg';
 import './Homepage.css';
-import Button from 'react-bootstrap/Button'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import ButtonSpot from './button/ButtonSpot';
 
 function Homepage() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Button>HI</Button>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <title>Eat HEre</title>
-      </header>
+        <ButtonSpot type="button" buttonText="Find Your Spot Today!" />
     </div>
   );
 }
