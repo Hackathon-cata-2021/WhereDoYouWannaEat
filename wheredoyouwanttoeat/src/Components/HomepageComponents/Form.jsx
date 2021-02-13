081df2d279e5d2393cf1392399fe6314bcdca4b0
Amir Sharapov <asharapov@catalyte.io>
2021-02-13 00:37:41 -0600

    add the form component

---
+++ wheredoyouwanttoeat/src/Components/HomepageComponents/Form.jsx
@@ -0,0 +1,17 @@
+const formComponent = () => {
+    return(
+        <div>
+            <form>
+                <label>
+                    What is your age?
+                </label>
+                <input/>
+                <label>
+                    What is your partner's age?
+                </label>
+                <input/>
+                <button>Submit</button>    
+            </form>
+        </div>
+    )
+}
