adc0b67da0cdb3993678b63665d05a3b558b9638
Lilly Tilden <lillymtilden@gmail.com>
2021-02-12 22:05:27 -0500

    added interest buttons

---
+++ wheredoyouwanttoeat/src/Components/Interests.jsx
@@ -0,0 +1,51 @@
+// import './Homepage.css';
+import Button from 'react-bootstrap/Button'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Homepage() {
+    return (
+        <div className="App">
+            <div id="food">
+                <Button>Sandwich</Button>
+                <Button>Salad</Button>
+                <Button>Asian</Button>
+                <Button>Italian</Button>
+                <Button>Mexican</Button>
+                <Button>Fast Food</Button>
+                <Button>Pizza</Button>
+                <Button>Chicken</Button>
+                <Button>Burgers</Button>
+                <Button>American</Button>
+            </div>
+
+            {/* <div id='books'>
+                <Button>Sandwich</Button>
+            </div> */}
+
+            {/* <div id='movies'>
+                <Button>Sandwich</Button>
+            </div> */}
+
+            {/* <div id='music'>
+                <Button>add music here</Button>
+            </div> */}
+
+            <div id='other'>
+                <Button>Art</Button>
+                <Button>Phylosophy</Button>
+                <Button>History</Button>
+                <Button>STEM</Button>
+                <Button>Beaches</Button>
+                <Button>Parks</Button>
+                <Button>Coffee</Button>
+                {/* will be changed to dropdowns */}
+                <Button>Books</Button>
+                <Button>Movies</Button>
+                <Button>Music</Button>
+            </div>
+
+        </div>
+    );
+}
+
+export default Homepage;
