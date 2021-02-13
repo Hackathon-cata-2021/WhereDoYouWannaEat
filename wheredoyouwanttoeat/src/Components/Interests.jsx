4ae14993a86bbbf66396c0c1d7ec851436e55a5b
Lilly Tilden <lillymtilden@gmail.com>
2021-02-13 03:00:47 -0500

    added functionality

--- wheredoyouwanttoeat/src/Components/Interests.jsx
+++ wheredoyouwanttoeat/src/Components/Interests.jsx
@@ -1,53 +1,58 @@
 // import './Homepage.css';
-import Button from 'react-bootstrap/Button'
+import InterestButton from './interest-button/InterestButton'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axiosCalls from './Utils';
+import {axiosCalls} from './Utils';
 
-function buttonOnClick(buttonValue){
-    let url = "https://maps.googleapis.com/maps/api/place/textsearch/json?input="+buttonValue+"&inputtype=textquery&fields=name,rating,opening_hours,place_id&radius=4000&key=AIzaSyCJoZQo8YwkU6LNHDWwMcPwd9DY5Kl4Neo";
-    const fetchData = async () =>{
+function setData(){};
+function buttonOnClick(buttonValue) {
+    let url = "https://maps.googleapis.com/maps/api/place/textsearch/json?input=" + buttonValue + "&inputtype=textquery&fields=name,rating,opening_hours,place_id&radius=4000&key=AIzaSyCJoZQo8YwkU6LNHDWwMcPwd9DY5Kl4Neo";
+    const fetchData = async () => {
         await axiosCalls(url)
-        .then((res) => (setData(res.data.results)));
+            .then((res) => (setData(res.data.results)));
+    }
 }
 
 function Interests() {
     return (
         <div className="App">
             <div id="food">
-                <Button onClick={()=> buttonOnClick('sandwich+shop')}>Sandwich</Button>
-                <Button onClick={()=> buttonOnClick('asian+restaurant')}>Asian</Button>
-                <Button onClick={()=> buttonOnClick('italian+restaurant')}>Italian</Button>
-                <Button onClick={()=> buttonOnClick('mexican+restaurant')}>Mexican</Button>
-                <Button onClick={()=> buttonOnClick('fast+food+restaurant')}>Fast Food</Button>
-                <Button onClick={()=> buttonOnClick('pizza+restaurant')}>Pizza</Button>
-                <Button onClick={()=> buttonOnClick('chicken+restaurant')}>Chicken</Button>
-                <Button onClick={()=> buttonOnClick('burger+restaurant')}>Burgers</Button>
-                <Button onClick={()=> buttonOnClick('american+restaurant')}>American</Button>
+                <h3>Food</h3>
+                <InterestButton type="button" buttonText="Sandwich" onClick={() => buttonOnClick('sandwich+shop')}/>
+                <InterestButton type="button" buttonText="Asian" onClick={() => buttonOnClick('asian+restaurant')}/>
+                <InterestButton type="button" buttonText="Italian" onClick={() => buttonOnClick('italian+restaurant')}/>
+                <InterestButton type="button" buttonText="Mexican" onClick={() => buttonOnClick('mexican+restaurant')}/>
+                <InterestButton type="button" buttonText="Fast Food" onClick={() => buttonOnClick('fast+food+restaurant')}/>
+                <InterestButton type="button" buttonText="Pizza" onClick={() => buttonOnClick('pizza+restaurant')}/>
+                <InterestButton type="button" buttonText="Chicken" onClick={() => buttonOnClick('chicken+restaurant')}/>
+                <InterestButton type="button" buttonText="Burgers" onClick={() => buttonOnClick('burger+restaurant')}/>
+                <InterestButton type="button" buttonText="American" onClick={() => buttonOnClick('american+restaurant')}/>
             </div>
-
+            <br />
             {/* <div id='books'>
-                <Button>Sandwich</Button>
+                <InterestButton type="button" buttonText="" >Sandwich</InterestButton>
             </div> */}
 
             {/* <div id='movies'>
-                <Button>Sandwich</Button>
+                <InterestButton type="button" buttonText="" >Sandwich</InterestButton>
             </div> */}
 
             {/* <div id='music'>
-                <Button>add music here</Button>
+                <InterestButton type="button" buttonText="" >add music here</InterestButton>
             </div> */}
 
             <div id='other'>
-                <Button onClick={()=> buttonOnClick('art+museum')}>Art</Button>
-                <Button onClick={()=> buttonOnClick('history+museum')}>History</Button>
-                <Button onClick={()=> buttonOnClick('science+museum')}>STEM</Button>
-                <Button onClick={()=> buttonOnClick('beach')}>Beaches</Button>
-                <Button onClick={()=> buttonOnClick('park')}>Parks</Button>
-                <Button onClick={()=> buttonOnClick('coffee+shop')}>Coffee</Button>
+                <h3>Other Stuffs</h3>
+
+                <InterestButton type="button" buttonText="Art" onClick={() => buttonOnClick('art+museum')}/>
+                <InterestButton type="button" buttonText="History" onClick={() => buttonOnClick('history+museum')}/>
+                <InterestButton type="button" buttonText="STEM" onClick={() => buttonOnClick('science+museum')}/>
+                <InterestButton type="button" buttonText="Beaches" onClick={() => buttonOnClick('beach')}/>
+                <InterestButton type="button" buttonText="Parks" onClick={() => buttonOnClick('park')}/>
+                <InterestButton type="button" buttonText="Coffee" onClick={() => buttonOnClick('coffee+shop')}/>
                 {/* will be changed to dropdowns */}
-                <Button onClick={()=> buttonOnClick('book+store')}>Books</Button>
-                <Button onClick={()=> buttonOnClick('movie+theater')}>Movies</Button>
-                <Button onClick={()=> buttonOnClick('music+venue')}>Music</Button>
+                <InterestButton type="button" buttonText="Books" onClick={() => buttonOnClick('book+store')}/>
+                <InterestButton type="button" buttonText="Movies" onClick={() => buttonOnClick('movie+theater')}/>
+                <InterestButton type="button" buttonText="Music" onClick={() => buttonOnClick('music+venue')}/>
             </div>
 
         </div>
