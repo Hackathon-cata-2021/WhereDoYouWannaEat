8be11e695ae2224d7cf8d80eae76bd591a767ba6
Lilly Tilden <lillymtilden@gmail.com>
2021-02-13 23:58:31 -0500

    added modal to cards

--- wheredoyouwanttoeat/src/Components/card-component/CardComponent.jsx
+++ wheredoyouwanttoeat/src/Components/card-component/CardComponent.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import Modal from 'react-bootstrap/Modal'
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -14,33 +15,58 @@ const useStyles = makeStyles({
   },
 });
 
-export default function ImgMediaCard( {cardHeading} ) {
+export default function ImgMediaCard({ cardHeading }) {
   const classes = useStyles();
 
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
-    <Card className={classes.root}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          alt="Contemplative Reptile"
-          height="140"
-          image="/static/images/cards/contemplative-reptile.jpg"
-          title="Contemplative Reptile"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="h2">
-            { cardHeading }
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-      <CardActions>
-        <Button size="small" color="primary">
-          Share
+    <div>
+      <Card className={classes.root} onClick={handleShow}>
+        <CardActionArea>
+          <CardMedia
+            component="img"
+            alt="Contemplative Reptile"
+            height="140"
+            image="/static/images/cards/contemplative-reptile.jpg"
+            title="Contemplative Reptile"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="h2">
+              {cardHeading}
+            </Typography>
+          </CardContent>
+        </CardActionArea>
+        <CardActions>
+          <Button size="small" color="primary">
+            Share
         </Button>
-        <Button size="small" color="primary">
-          Learn More
+          <Button size="small" color="primary">
+            Learn More
         </Button>
-      </CardActions>
-    </Card>
+        </CardActions>
+      </Card>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you're reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      
+    </div>
+
   );
 }
+
