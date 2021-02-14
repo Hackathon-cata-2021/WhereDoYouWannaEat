93e60a8cbdbb8b28c6f309f94d12ab18ab4339dd
Amir Sharapov <asharapov@catalyte.io>
2021-02-14 00:27:14 -0600

    remove share button and image

--- wheredoyouwanttoeat/src/Components/card-component/CardComponent.jsx
+++ wheredoyouwanttoeat/src/Components/card-component/CardComponent.jsx
@@ -50,13 +50,6 @@ export default function ImgMediaCard({ cardHeading, placeId }) {
     <div>
       <Card className={classes.root} onClick={handleShow}>
         <CardActionArea>
-          <CardMedia
-            component="img"
-            alt="Contemplative Reptile"
-            height="140"
-            image="/static/images/cards/contemplative-reptile.jpg"
-            title="Contemplative Reptile"
-          />
           <CardContent>
             <Typography gutterBottom variant="h5" component="h2">
               {cardHeading}
@@ -64,9 +57,6 @@ export default function ImgMediaCard({ cardHeading, placeId }) {
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Button size="small" color="primary">
-            Share
-        </Button>
           <Button size="small" color="primary">
             Learn More
         </Button>
